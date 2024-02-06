4894f3f7b341fd2de67229a534b046143ccf0366
mhd-zaid <zaidmouhamad@gmail.com>
2024-02-06 17:43:50 +0100

    remove server from docker

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 import http from "http";
+import dotenv from "dotenv";
 import app from "./app.js";
-
+dotenv.config({path: ".env.local"});
 const PORT = process.env.PORT || 3000;
 app.set("port", PORT);
 const server = http.createServer(app);
