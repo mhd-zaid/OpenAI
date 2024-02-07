8315c46e81674e27c3d20a39f34b6d28ee7afc0a
dan1M <maneadaniel555@gmail.com>
2024-02-07 14:25:37 +0100

    cleaning

--- server/server.js
+++ server/server.js
@@ -1,7 +1,5 @@
 import http from 'http';
 import app from './app.js';
-import dotenv from 'dotenv';
-dotenv.config({ path: '.env.local' });
 
 const PORT = process.env.PORT || 3000;
 app.set('port', PORT);
