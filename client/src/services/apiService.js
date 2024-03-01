63452eabd1aad2a48c68838e0755c65aa794f02b
Makan Kamissoko <makan.kamissoko@hotmail.fr>
2024-03-01 16:02:53 +0100

    create an apiService, formValidator and utils for token management

---
+++ client/src/services/apiService.js
@@ -0,0 +1,40 @@
+const API_URL_BASE = import.meta.env.VITE_API_URL;
+const defaultHeaders = {
+    method: "GET",
+    headers: new Headers({ "Content-Type": "application/json" }),
+    body: null,
+};
+
+export const apiService = {
+
+    getAll(instance, params){
+        let url = `${API_URL_BASE}/${instance}?${params}`;
+        return fetch(url, { ...defaultHeaders })
+            .then((response) => response.json());
+    },
+
+    getOne(instance, id){
+        return fetch(`${API_URL_BASE}/${instance}/${id}`, { ...defaultHeaders })
+            .then((response) => response.json());
+    },
+
+    create(instance, data){
+        defaultHeaders.method = "POST";
+        defaultHeaders.body = JSON.stringify(data);
+        return fetch(`${API_URL_BASE}/${instance}`, { ...defaultHeaders })
+            .then((response) => response.json());
+    },
+
+    update(instance, id, data){
+        defaultHeaders.method = "PUT";
+        defaultHeaders.body = JSON.stringify(data);
+        return fetch(`${API_URL_BASE}/${instance}/${id}`, { ...defaultHeaders })
+            .then((response) => response.json());
+    },
+
+    deleteById(instance, id){
+        defaultHeaders.method = "DELETE";
+        return fetch(`${API_URL_BASE}/${instance}/${id}`, { ...defaultHeaders })
+            .then((response) => response.json());
+    }
+}
