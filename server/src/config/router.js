6d1c9e3d034e52b0ad267c163ea31701b1c8db01
dan1M <maneadaniel555@gmail.com>
2024-03-01 15:43:09 +0100

    getting chatbot its own service (instead of generic openai service)

--- server/src/config/router.js
+++ server/src/config/router.js
@@ -1,6 +1,6 @@
 import authRoutes from '../routes/auth.js';
 import chatbotRoutes from '../routes/chatbot.js';
-export default (app,express) => {
-    app.use('/api', authRoutes(express.Router()));
-    app.use('/api/chatbot', chatbotRoutes(express.Router()));
-}
+export default (app, express) => {
+  app.use('/api', authRoutes(express.Router()));
+  app.use('/api/chatbot', chatbotRoutes(express.Router()));
+};
