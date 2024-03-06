93981a3351e826d0dc1c55d524b6ea69256c8fd9
mhd-zaid <zaidmouhamad@gmail.com>
2024-03-06 21:19:24 +0100

    recipe quantity fixutres non fonctionnel

--- server/src/fixtures/index.js
+++ server/src/fixtures/index.js
@@ -1,25 +1,90 @@
 import connection from '../config/sequelize.js';
 // Fixtures
-import ingredientFixture from "./ingredient.js";
+import ingredientFixture from './ingredient.js';
+import recipeFixture from './recipe.js';
 
 const loadIngredients = async () => {
-    const model = (await import("../models/Ingredient.js")).default(connection)
-	try {
-		await Promise.all(ingredientFixture.map((ingredient) => model.create(ingredient)));
-		console.log("Ingredients loaded");
-	} catch (err) {
-		console.error(err);
-	}
+  const model = (await import('../models/Ingredient.js')).default(connection);
+  try {
+    await Promise.all(
+      ingredientFixture.map(ingredient => model.create(ingredient)),
+    );
+    console.log('Ingredients loaded');
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+const loadRecipes = async () => {
+  const model = (await import('../models/Recipe.js')).default(connection);
+  try {
+    await Promise.all(recipeFixture.map(recipe => model.create(recipe)));
+    console.log('Recipes loaded');
+  } catch (err) {
+    console.error(err);
+  }
+};
+const getRecipeByName = async name => {
+  const model = (await import('../models/Recipe.js')).default(connection);
+  try {
+    const recipe = await model.findOne({ where: { title: name } });
+    return recipe;
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+const getIngredientByName = async name => {
+  const model = (await import('../models/Ingredient.js')).default(connection);
+  try {
+    const ingredient = await model.findOne({ where: { name: name } });
+    return ingredient;
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+const loadQuantity = async () => {
+  const model = (await import('../models/Quantity.js')).default(connection);
+  try {
+    let recipe = await getRecipeByName('Omelette aux tomates et au fromage');
+    let ingredients = [
+      {
+        quantity: 2,
+        unit: null,
+        Ingredient: await getIngredientByName('Oeuf'),
+        Recipe: recipe,
+      },
+      {
+        quantity: 50,
+        unit: 'g',
+        Ingredient: await getIngredientByName('Fromage'),
+        Recipe: recipe,
+      },
+      {
+        quantity: 1,
+        unit: null,
+        Ingredient: await getIngredientByName('Tomate'),
+        Recipe: recipe,
+      },
+    ];
+    await Promise.all(ingredients.map(ingredient => model.create(ingredient)));
+    console.log('Quantities loaded');
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 const main = async () => {
-	try {
-		await loadIngredients();
-	} catch (error) {
-		console.error(error);
-	} finally {
-		connection.close();
-	}
+  try {
+    await loadIngredients();
+    await loadRecipes();
+    await loadQuantity();
+  } catch (error) {
+    console.error(error);
+  } finally {
+    connection.close();
+  }
 };
 
 main();
--- server/src/fixtures/ingredient.js
+++ server/src/fixtures/ingredient.js
@@ -1,271 +1,274 @@
 import { v4 as uuidv4 } from 'uuid';
 
 const ingredientFixture = [
-    {
-        id: uuidv4(),
-        name: "Farine",
-    },
-    {
-        id: uuidv4(),
-        name: "Sucre",
-    },
-    {
-        id: uuidv4(),
-        name: "Oeuf",
-    },
-    {
-        id: uuidv4(),
-        name: "Lait",
-    },
-    {
-        id: uuidv4(),
-        name: "Beurre",
-    },
-    {
-        id: uuidv4(),
-        name: "Chocolat",
-    },
-    {
-        id: uuidv4(),
-        name: "Levure",
-    },
-    {
-        id: uuidv4(),
-        name: "Vanille",
-    },
-    {
-        id: uuidv4(),
-        name: "Crème fraîche",
-    },
-    {
-        id: uuidv4(),
-        name:"Crème liquide",
-    },
-    {
-        id: uuidv4(),
-        name: "Sel",
-    },
-    {
-        id: uuidv4(),
-        name: "mascarpone",
-    },
-    {
-        id: uuidv4(),
-        name: "Fraise",
-    },
-    {
-        id: uuidv4(),
-        name: "Banane",
-    },
-    {
-        id: uuidv4(),
-        name: "Pomme",
-    },
-    {
-        id: uuidv4(),
-        name: "Poire",
-    },
-    {
-        id: uuidv4(),
-        name: "Ananas",
-    },
-    {
-        id: uuidv4(),
-        name: "Mangue",
-    },
-    {
-        id: uuidv4(),
-        name: "Fruit de la passion",
-    },
-    {
-        id: uuidv4(),
-        name: "Kiwi",
-    },
-    {
-        id: uuidv4(),
-        name: "Framboise",
-    },
-    {
-        id: uuidv4(),
-        name: "Cerise",
-    },
-    {
-        id: uuidv4(),
-        name: "Abricot",
-    },
-    {
-        id: uuidv4(),
-        name: "Orange",
-    },
-    {
-        id: uuidv4(),
-        name: "Citron",
-    },
-    {
-        id: uuidv4(),
-        name: "Pêche",
-    },
-    {
-        id: uuidv4(),
-        name: "Raisin",
-    },
-    {
-        id: uuidv4(),
-        name: "Grenade",
-    },
-    {
-        id: uuidv4(),
-        name: "Myrtille",
-    },
-    {
-        id: uuidv4(),
-        name: "Cassis",
-    },
-    {
-        id: uuidv4(),
-        name: "Mûre",
-    },
-    {
-        id: uuidv4(),
-        name: "Figue",
-    },
-    {
-        id: uuidv4(),
-        name: "Papaye",
-    },
-    {
-        id: uuidv4(),
-        name: "Goyave",
-    },
-    {
-        id: uuidv4(),
-        name: "Litchi",
-    },
-    {
-        id: uuidv4(),
-        name: "Melon",
-    },
-    {
-        id: uuidv4(),
-        name: "Pastèque",
-    },
-    {
-        id: uuidv4(),
-        name: "Carotte",
-    },
-    {
-        id: uuidv4(),
-        name: "Pomme de terre",
-    },
-    {
-        id: uuidv4(),
-        name: "Tomate",
-    },
-    {
-        id: uuidv4(),
-        name: "Concombre",
-    },
-    {
-        id: uuidv4(),
-        name: "Courgette",
-    },
-    {
-        id: uuidv4(),
-        name: "Aubergine",
-    },
-    {
-        id: uuidv4(),
-        name: "Poivron",
-    },
-    {
-        id: uuidv4(),
-        name: "Oignon",
-    },
-    {
-        id: uuidv4(),
-        name: "Ail",
-    },
-    {
-        id: uuidv4(),
-        name: "Echalotte",
-    },
-    {
-        id: uuidv4(),
-        name: "Chou",
-    },
-    {
-        id: uuidv4(),
-        name: "Salade",
-    },
-    {
-        id: uuidv4(),
-        name: "Epinard",
-    },
-    {
-        id: uuidv4(),
-        name: "Haricot",
-    },
-    {
-        id: uuidv4(),
-        name: "Petit pois",
-    },
-    {
-        id: uuidv4(),
-        name: "Pois chiche",
-    },
-    {
-        id: uuidv4(),
-        name: "Lentille",
-    },
-    {
-        id: uuidv4(),
-        name: "Fève",
-    },
-    {
-        id: uuidv4(),
-        name: "Champignon",
-    },
-    {
-        id: uuidv4(),
-        name: "Radis",
-    },
-    {
-        id: uuidv4(),
-        name: "Navet",
-    },
-    {
-        id: uuidv4(),
-        name: "Betterave",
-    },
-    {
-        id: uuidv4(),
-        name: "Panais",
-    },
-    {
-        id: uuidv4(),
-        name: "Céleri",
-    },
-    {
-        id: uuidv4(),
-        name: "Courge",
-    },
-    {
-        id: uuidv4(),
-        name: "Potiron",
-    },
-    {
-        id: uuidv4(),
-        name: "Patate"
-    },
-    {
-        id: uuidv4(),
-        name: "Tablette de chocolat",
-    },
-    {
-        id: uuidv4(),
-        name: "Tablette de chocolat blanc",
-    }
-
+  {
+    id: uuidv4(),
+    name: 'Farine',
+  },
+  {
+    id: uuidv4(),
+    name: 'Sucre',
+  },
+  {
+    id: uuidv4(),
+    name: 'Oeuf',
+  },
+  {
+    id: uuidv4(),
+    name: 'Lait',
+  },
+  {
+    id: uuidv4(),
+    name: 'Beurre',
+  },
+  {
+    id: uuidv4(),
+    name: 'Chocolat',
+  },
+  {
+    id: uuidv4(),
+    name: 'Levure',
+  },
+  {
+    id: uuidv4(),
+    name: 'Vanille',
+  },
+  {
+    id: uuidv4(),
+    name: 'Crème fraîche',
+  },
+  {
+    id: uuidv4(),
+    name: 'Crème liquide',
+  },
+  {
+    id: uuidv4(),
+    name: 'Sel',
+  },
+  {
+    id: uuidv4(),
+    name: 'mascarpone',
+  },
+  {
+    id: uuidv4(),
+    name: 'Fraise',
+  },
+  {
+    id: uuidv4(),
+    name: 'Banane',
+  },
+  {
+    id: uuidv4(),
+    name: 'Pomme',
+  },
+  {
+    id: uuidv4(),
+    name: 'Poire',
+  },
+  {
+    id: uuidv4(),
+    name: 'Ananas',
+  },
+  {
+    id: uuidv4(),
+    name: 'Mangue',
+  },
+  {
+    id: uuidv4(),
+    name: 'Fruit de la passion',
+  },
+  {
+    id: uuidv4(),
+    name: 'Kiwi',
+  },
+  {
+    id: uuidv4(),
+    name: 'Framboise',
+  },
+  {
+    id: uuidv4(),
+    name: 'Cerise',
+  },
+  {
+    id: uuidv4(),
+    name: 'Abricot',
+  },
+  {
+    id: uuidv4(),
+    name: 'Orange',
+  },
+  {
+    id: uuidv4(),
+    name: 'Citron',
+  },
+  {
+    id: uuidv4(),
+    name: 'Pêche',
+  },
+  {
+    id: uuidv4(),
+    name: 'Raisin',
+  },
+  {
+    id: uuidv4(),
+    name: 'Grenade',
+  },
+  {
+    id: uuidv4(),
+    name: 'Myrtille',
+  },
+  {
+    id: uuidv4(),
+    name: 'Cassis',
+  },
+  {
+    id: uuidv4(),
+    name: 'Mûre',
+  },
+  {
+    id: uuidv4(),
+    name: 'Figue',
+  },
+  {
+    id: uuidv4(),
+    name: 'Papaye',
+  },
+  {
+    id: uuidv4(),
+    name: 'Goyave',
+  },
+  {
+    id: uuidv4(),
+    name: 'Litchi',
+  },
+  {
+    id: uuidv4(),
+    name: 'Melon',
+  },
+  {
+    id: uuidv4(),
+    name: 'Pastèque',
+  },
+  {
+    id: uuidv4(),
+    name: 'Carotte',
+  },
+  {
+    id: uuidv4(),
+    name: 'Pomme de terre',
+  },
+  {
+    id: uuidv4(),
+    name: 'Tomate',
+  },
+  {
+    id: uuidv4(),
+    name: 'Concombre',
+  },
+  {
+    id: uuidv4(),
+    name: 'Courgette',
+  },
+  {
+    id: uuidv4(),
+    name: 'Aubergine',
+  },
+  {
+    id: uuidv4(),
+    name: 'Poivron',
+  },
+  {
+    id: uuidv4(),
+    name: 'Oignon',
+  },
+  {
+    id: uuidv4(),
+    name: 'Ail',
+  },
+  {
+    id: uuidv4(),
+    name: 'Echalotte',
+  },
+  {
+    id: uuidv4(),
+    name: 'Chou',
+  },
+  {
+    id: uuidv4(),
+    name: 'Salade',
+  },
+  {
+    id: uuidv4(),
+    name: 'Epinard',
+  },
+  {
+    id: uuidv4(),
+    name: 'Haricot',
+  },
+  {
+    id: uuidv4(),
+    name: 'Petit pois',
+  },
+  {
+    id: uuidv4(),
+    name: 'Pois chiche',
+  },
+  {
+    id: uuidv4(),
+    name: 'Lentille',
+  },
+  {
+    id: uuidv4(),
+    name: 'Fève',
+  },
+  {
+    id: uuidv4(),
+    name: 'Champignon',
+  },
+  {
+    id: uuidv4(),
+    name: 'Radis',
+  },
+  {
+    id: uuidv4(),
+    name: 'Navet',
+  },
+  {
+    id: uuidv4(),
+    name: 'Betterave',
+  },
+  {
+    id: uuidv4(),
+    name: 'Panais',
+  },
+  {
+    id: uuidv4(),
+    name: 'Céleri',
+  },
+  {
+    id: uuidv4(),
+    name: 'Courge',
+  },
+  {
+    id: uuidv4(),
+    name: 'Potiron',
+  },
+  {
+    id: uuidv4(),
+    name: 'Patate',
+  },
+  {
+    id: uuidv4(),
+    name: 'Tablette de chocolat',
+  },
+  {
+    id: uuidv4(),
+    name: 'Tablette de chocolat blanc',
+  },
+  {
+    id: uuidv4(),
+    name: 'Fromage',
+  },
 ];
 
 export default ingredientFixture;
