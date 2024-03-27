3d7d4caa0f4c1793068423e6df2e3a4aa927bae7
mhd-zaid <zaidmouhamad@gmail.com>
2024-03-27 11:50:30 +0100

    fix aiSearch prompt + facto

--- server/src/routes/favorite.js
+++ server/src/routes/favorite.js
@@ -1,26 +1,24 @@
-
-import GenericRouter from "../routes/genericRouter.js";
-import GenericController from "../controllers/genericController.js";
-import GenericService from "../Services/genericService.js";
-import db from "../../src/models/index.js";
+import GenericRouter from '../routes/genericRouter.js';
+import GenericController from '../controllers/genericController.js';
+import GenericService from '../services/genericService.js';
+import db from '../../src/models/index.js';
 import favoriteController from '../controllers/favorite.js';
 const genericRoutes = [
-    { method: 'GET', path: '/:id', handler: 'getById', middlewares: [] },
-    { method: 'POST', path: '/', handler: 'create', middlewares: [] },
-    { method: 'PUT', path: '/:id', handler: 'update', middlewares: [] },
-    { method: 'PATCH', path: '/:id', handler: 'patch', middlewares: [] },
-    { method: 'DELETE', path: '/:id', handler: 'delete', middlewares: [] },
-  ];
+  { method: 'GET', path: '/:id', handler: 'getById', middlewares: [] },
+  { method: 'POST', path: '/', handler: 'create', middlewares: [] },
+  { method: 'PUT', path: '/:id', handler: 'update', middlewares: [] },
+  { method: 'PATCH', path: '/:id', handler: 'patch', middlewares: [] },
+  { method: 'DELETE', path: '/:id', handler: 'delete', middlewares: [] },
+];
 
-
-  const genericFavoriteRouter = new GenericRouter(
+const genericFavoriteRouter = new GenericRouter(
   new GenericController(new GenericService(db.Favorite)),
 );
 genericRoutes.forEach(route => {
   genericFavoriteRouter.addRoute(route, route.middlewares);
 });
 
-export default (router) => {
+export default router => {
   router.use('/', genericFavoriteRouter.getRouter());
   router.get('/user/:id', favoriteController.getAllByUser);
   return router;
--- server/src/routes/quantity.js
+++ server/src/routes/quantity.js
@@ -1,27 +1,25 @@
-
-import GenericRouter from "../routes/genericRouter.js";
-import GenericController from "../controllers/genericController.js";
-import GenericService from "../Services/genericService.js";
-import db from "../../src/models/index.js";
+import GenericRouter from '../routes/genericRouter.js';
+import GenericController from '../controllers/genericController.js';
+import GenericService from '../services/genericService.js';
+import db from '../../src/models/index.js';
 
 const genericRoutes = [
-    { method: 'GET', path: '/', handler: 'getAll', middlewares: [] },
-    { method: 'GET', path: '/:id', handler: 'getById', middlewares: [] },
-    { method: 'POST', path: '/', handler: 'create', middlewares: [] },
-    { method: 'PUT', path: '/:id', handler: 'update', middlewares: [] },
-    { method: 'PATCH', path: '/:id', handler: 'patch', middlewares: [] },
-    { method: 'DELETE', path: '/:id', handler: 'delete', middlewares: [] },
-  ];
+  { method: 'GET', path: '/', handler: 'getAll', middlewares: [] },
+  { method: 'GET', path: '/:id', handler: 'getById', middlewares: [] },
+  { method: 'POST', path: '/', handler: 'create', middlewares: [] },
+  { method: 'PUT', path: '/:id', handler: 'update', middlewares: [] },
+  { method: 'PATCH', path: '/:id', handler: 'patch', middlewares: [] },
+  { method: 'DELETE', path: '/:id', handler: 'delete', middlewares: [] },
+];
 
-
-  const genericQuantityRouter = new GenericRouter(
+const genericQuantityRouter = new GenericRouter(
   new GenericController(new GenericService(db.Quantity)),
 );
 genericRoutes.forEach(route => {
-    genericQuantityRouter.addRoute(route, route.middlewares);
+  genericQuantityRouter.addRoute(route, route.middlewares);
 });
 
-export default (router) => {
+export default router => {
   router.use('/', genericQuantityRouter.getRouter());
   return router;
 };
